b46860976184d5d9cdf6ac1c83456f3a12a65b8c
mgrinko <glukym@gmail.com>
2016-01-16 03:43:32 +0800

    [navigation] - filter and menu were added as subcomponents

--- app.js
+++ app.js
@@ -13,4 +13,54 @@ class Menu {
     console.log(this);
     this._el.classList.toggle('open');
   }
+
+  filter(value) {
+    console.log(`Menu was filtered with "${value}"`);
+  }
+}
+
+class Filter {
+  constructor(options) {
+    this._el = options.element;
+
+    this._field = this._el.querySelector('.filter_field');
+
+    this._field.addEventListener('input', this._onFieldInput.bind(this));
+  }
+
+  getElement() {
+    return this._el;
+  }
+
+  _onFieldInput(event) {
+    var customEvent = new CustomEvent('filterValueChanged', {
+      detail: {
+        value: this._field.value
+      }
+    });
+
+    console.log('Filter value has changed to ' + this._field.value);
+
+    this._el.dispatchEvent(customEvent);
+  }
+}
+
+class Navigation {
+  constructor(options) {
+    this._el = options.element;
+
+    this._filter = new Filter({
+      element: this._el.querySelector('.filter')
+    });
+
+    this._menu = new Menu({
+      element: this._el.querySelector('.menu')
+    });
+
+    this._filter.getElement().addEventListener('filterValueChanged', this._onFilterValueChanged.bind(this))
+  }
+
+  _onFilterValueChanged(event) {
+    console.log('Current filter value is ' + event.detail.value);
+  }
 }
