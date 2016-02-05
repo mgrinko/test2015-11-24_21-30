e9e91aad21af455b6491828374ea93275e4b8c32
mgrinko <glukym@gmail.com>
2016-02-05 20:45:52 +0200

    ajax basic

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -4,9 +4,8 @@ require('./../styles/main.css');
 
 let Page = require('./page/index');
 
-let phones = require('./../../data/phones.json');
 
 let nav = new Page({
   element: document.getElementById('application'),
-  phones: phones
+  phonesUrl: '/data/phones.json'
 });
--- frontend/scripts/page/index.js
+++ frontend/scripts/page/index.js
@@ -6,7 +6,29 @@ let PhoneCatalog = require('./../phoneCatalog');
 module.exports = class Page {
   constructor(options) {
     this._el = options.element;
+    this._el.classList.add('loading');
 
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', options.phonesUrl, true);
+
+
+    xhr.onload = function() {
+      this._el.classList.remove('loading');
+
+      if (xhr.status !== 200) {
+        console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+      } else {
+        this._init({
+          phones: JSON.parse(xhr.responseText)
+        });
+      }
+    }.bind(this);
+
+    xhr.send();
+  }
+
+  _init(options) {
     this._filter = new Filter({
       element: this._el.querySelector('[data-component=filter]')
     });
